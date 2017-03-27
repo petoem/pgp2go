d9f36056b266174ba834426667bd4297ff0dc1a1
Michael Petö <michael@petoe.me>
2017-03-27 14:30:58 +0200

    Changes in webpack config
    - use enviroment variables
    - uglify and subresource integrity only in production
    - minify html and copy css to output location

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+const SriPlugin = require('webpack-subresource-integrity');
+
+let plugins = [
+    new CopyWebpackPlugin([{
+        from: 'src/style.css',
+        to: 'style.css'
+    }]),
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+        inject: 'body',
+        minify: (process.env.NODE_ENV === 'production') ? {
+            collapseWhitespace: true,
+            conservativeCollapse: true
+        } : false,
+        hash: true,
+        cache: false,
+        showErrors: false
+    }),
+    new HtmlWebpackIncludeAssetsPlugin({
+        append: true,
+        hash: true,
+        assets: [
+            'style.css'
+        ]
+    })
+];
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(new UglifyJSPlugin({
+        mangle: false,
+        sousourceMap: false
+    }));
+    plugins.push(new SriPlugin({
+        hashFuncNames: ['sha256', 'sha384'],
+        enabled: process.env.NODE_ENV === 'production'
+    }));
+}
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, (process.env.OUT_DIR) ? process.env.OUT_DIR : './build'),
+        filename: (process.env.NODE_ENV === 'production') ? 'bundle.[hash].js' : 'bundle.js',
+        crossOriginLoading: 'anonymous'
+    },
+    plugins: plugins
 }
